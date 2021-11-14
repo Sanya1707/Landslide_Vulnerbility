ccf60ff3aa3b508cc68f92b2613cffa0f8f0bc79
Sanya1707 <94275577+Sanya1707@users.noreply.github.com>
2021-11-14 10:33:47 +0530

    Add files via upload

---
+++ data/Earthquake_12.js
@@ -0,0 +1,11 @@
+var json_Earthquake_12 = {
+"type": "FeatureCollection",
+"name": "Earthquake_12",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": "623", "SOURCE": "CGS", "YR": 1958.0, "MO": 12.0, "DT": 28.0, "HR": 5.0, "MN": 34.0, "SEC": 36.0, "LAT": 29.5, "LONG_": 80.0, "MAGMB": 5.8, "DEPTH_KM": 0.0, "MW": 5.96 }, "geometry": { "type": "Point", "coordinates": [ 80.0, 29.5 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "4600", "SOURCE": "ISC", "YR": 1980.0, "MO": 1.0, "DT": 16.0, "HR": 20.0, "MN": 9.0, "SEC": 35.0, "LAT": 29.51, "LONG_": 80.0, "MAGMB": 4.8, "DEPTH_KM": 141.0, "MW": 4.8 }, "geometry": { "type": "Point", "coordinates": [ 80.0, 29.51 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "6822", "SOURCE": "ISC", "YR": 1985.0, "MO": 6.0, "DT": 14.0, "HR": 17.0, "MN": 19.0, "SEC": 7.0, "LAT": 29.700001, "LONG_": 79.400002, "MAGMB": 3.8, "DEPTH_KM": 33.0, "MW": 4.26 }, "geometry": { "type": "Point", "coordinates": [ 79.400002, 29.700001 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "11431", "SOURCE": "ISC", "YR": 1995.0, "MO": 7.0, "DT": 15.0, "HR": 6.0, "MN": 39.0, "SEC": 43.0, "LAT": 29.9, "LONG_": 79.400002, "MAGMB": 4.2, "DEPTH_KM": 33.0, "MW": 4.6 }, "geometry": { "type": "Point", "coordinates": [ 79.400002, 29.9 ] } }
+]
+}
---
+++ data/School_11.js
@@ -0,0 +1,9 @@
+var json_School_11 = {
+"type": "FeatureCollection",
+"name": "School_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "full_id": "n4177522403", "osm_id": "4177522403", "osm_type": "node", "amenity": "school", "name": "Government College", "addr_stree": "chiliyanaula", "name_en": null, "name_hi": null, "addr_postc": null }, "geometry": { "type": "Point", "coordinates": [ 79.4158683, 29.6585093 ] } },
+{ "type": "Feature", "properties": { "full_id": "n4852903921", "osm_id": "4852903921", "osm_type": "node", "amenity": "school", "name": "GMSFP SCHOOL", "addr_stree": "Ranikhet", "name_en": "GMSFP SCHOOL", "name_hi": "????? ???????", "addr_postc": "263645" }, "geometry": { "type": "Point", "coordinates": [ 79.4340514, 29.6397894 ] } }
+]
+}
